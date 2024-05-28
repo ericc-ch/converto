2499beb062fa98a43a82449ae05db23c5611d782
Erick Christian <erickchristian48@gmail.com>
2024-05-28 08:27:44 +0700

    feat: [wip] form

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,5 +1,6 @@
-import { Box, Radio, Text, Title } from "@mantine/core";
+import { Box, Radio, Stack, Text, Title } from "@mantine/core";
 import { Dropzone, IMAGE_MIME_TYPE } from "@mantine/dropzone";
+import { useForm } from "@mantine/form";
 import type { MetaFunction } from "@remix-run/cloudflare";
 import { z } from "zod";
 import { megabytes } from "~/utils/size";
@@ -18,33 +19,58 @@ const SIZE_MAX = megabytes(20);
 const OPTIONS = ["jpeg", "png", "webp", "avif"] as const;
 
 const schema = z.object({
+  images: z
+    .array(
+      z.object({
+        size: z.number().min(0).max(SIZE_MAX),
+      })
+    )
+    .min(1),
   output: z.enum(OPTIONS),
 });
 
 export default function Index() {
+  const form = useForm()
+
   return (
-    <Box>
-      <Title order={1}>File Converter</Title>
-      <Text>Dead simple, just drop your file.</Text>
+    <Box style={{ paddingInline: "1rem", paddingBlockStart: "4rem" }}>
+      <Stack gap="xl" w="min(400px, 100%)" mx="auto">
+        <Box>
+          <Title ta="center" order={1}>
+            File Converter
+          </Title>
+          <Text ta="center">Dead simple, just drop your file.</Text>
+        </Box>
 
-      <Dropzone
-        maxSize={SIZE_MAX}
-        onDrop={console.log}
-        accept={IMAGE_MIME_TYPE}
-      >
-        Drop
-      </Dropzone>
+        <Dropzone
+          maxSize={SIZE_MAX}
+          onDrop={console.log}
+          accept={IMAGE_MIME_TYPE}
+        >
+          <Box h={144}>
+            <Dropzone.Idle>
+              <Text>Drop it like its hot</Text>
+            </Dropzone.Idle>
 
-      <Radio.Group
-        name="output"
-        label="Output Format"
-        description="Select the format you want your image in."
-        withAsterisk
-      >
-        {OPTIONS.map((option) => (
-          <Radio key={option} value={option} label={option} />
-        ))}
-      </Radio.Group>
+            <Dropzone.Accept>Yep that seems good</Dropzone.Accept>
+
+            <Dropzone.Reject>Nah man, check the file type</Dropzone.Reject>
+          </Box>
+        </Dropzone>
+
+        <Radio.Group
+          name="output"
+          label="Output Format"
+          description="Select the format you want your image in."
+          withAsterisk
+        >
+          <Stack mt="sm" gap="sm">
+            {OPTIONS.map((option) => (
+              <Radio key={option} value={option} label={option} />
+            ))}
+          </Stack>
+        </Radio.Group>
+      </Stack>
     </Box>
   );
 }
